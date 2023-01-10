370959a4466dc994492bd4f9453e178d56f92da0
Andy Lang <djdreviz@gmail.com>
2023-01-10 22:04:13 +0200

    47. Dynamically Generating UI from an Array valmis!

--- react-movies/src/index.tsx
+++ react-movies/src/index.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import ConditionalsIf from "./ConditionalsIf";
+import MapExample from "./MapExample";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ConditionalsIf />
+    <MapExample />
   </React.StrictMode>
 );
 
