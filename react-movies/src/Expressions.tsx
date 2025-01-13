44d837be372463f00b9eba9e2ce9eab14aec769c
Andy Lang <djdreviz@gmail.com>
2025-01-13 23:02:17 +0200

    41. Expressions in JSX done!

---
+++ react-movies/src/Expressions.tsx
@@ -0,0 +1,10 @@
+export default function Expressions() {
+    const dublicate = (value: number) => value*2;
+  return (
+    
+    <>
+      <h3>Example Expressions</h3>
+      <div>Number of 3.65 dublicate is: {dublicate(3.65)}</div>
+    </>
+  );
+}
