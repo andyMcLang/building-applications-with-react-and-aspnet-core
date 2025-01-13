e58373c7b1049faa6a5b06d81e3312c8fd9ffceb
Andy Lang <djdreviz@gmail.com>
2025-01-14 00:24:58 +0200

    45. Using the useState React Hook done!

--- react-movies/src/Events.tsx
+++ react-movies/src/Events.tsx
@@ -1,12 +1,26 @@
+import { useState } from "react";
+
 export default function Events() {
-    function handleKeyUp(e: React.KeyboardEvent<HTMLInputElement>) {
-        console.log(e.currentTarget.value)
-    }
-    
+  const [teksti, setTeksti] = useState("");
+  let [voitNahda, setVoitNahda] = useState(false);
+
+  function handleClickUp(e: React.KeyboardEvent<HTMLInputElement>) {
+    setTeksti(e.currentTarget.value);
+  }
+
+  function arvonVaihto() {
+    setVoitNahda(!voitNahda);
+  }
+
   return (
     <>
       <h1>Events Example</h1>
-      <input type="text" onKeyUp={handleKeyUp} />
+      <input type="checkbox" onChange={arvonVaihto} />
+      <div>{voitNahda ? "voit nähdä" : "et voi nähdä!"}</div>
+      <div>
+        <input type="text" onKeyUp={(e) => handleClickUp(e)} />
+        <div>{teksti}</div>
+      </div>
     </>
   );
 }
