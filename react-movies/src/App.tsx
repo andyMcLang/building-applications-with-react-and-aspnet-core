899a63079654e6a41609c6948f3a4e04539628ef
Andy Lang <djdreviz@gmail.com>
2025-02-01 03:32:33 +0200

    63. Making the MoviesList Component More Robust done!

--- react-movies/src/App.tsx
+++ react-movies/src/App.tsx
@@ -1,45 +1,56 @@
+import { useEffect, useState } from "react";
 import "./App.css";
-import { movieDTO } from "./movies/movies.model";
+import { landingPageDTO, movieDTO } from "./movies/movies.model";
 import MoviesList from "./movies/MoviesList";
 
 function App() {
-  const inTheaters: movieDTO[] = [
-    {
-      id: 1,
-      title: "Nosferatu",
-      poster:
-        "https://media.finnkino.fi/1012/Event_14344/portrait_medium/Nosferatu_1080_v2.jpg",
-    },
-    {
-      id: 2,
-      title: "Levoton Tuhkimo",
-      poster:
-        "https://media.finnkino.fi/1012/Event_14300/portrait_medium/LevotonTuhkimo_1080.jpg",
-    },
-  ];
+  const [movies, setMovies] = useState<landingPageDTO>({})
 
-  const tulossaLeffat: movieDTO[] = [
-    {
-      id: 1,
-      title: "Mission: Impossible - The Final Reckoning",
-      poster:
-        "https://media.finnkino.fi/1012/Event_14435/portrait_medium/MissionImpossibleTFR_1080.jpg",
-    },
-    {
-      id: 2,
-      title: "Karate Kid: Legends",
-      poster:
-        "https://media.finnkino.fi/1012/Event_14477/portrait_medium/KarateKidLegends_1080.jpg",
-    },
-  ];
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setMovies({
+        ohjelmistossaNyt: [
+          {
+            id: 1,
+            title: "Nosferatu",
+            poster:
+              "https://media.finnkino.fi/1012/Event_14344/portrait_medium/Nosferatu_1080_v2.jpg",
+          },
+          {
+            id: 2,
+            title: "Levoton Tuhkimo",
+            poster:
+              "https://media.finnkino.fi/1012/Event_14300/portrait_medium/LevotonTuhkimo_1080.jpg",
+          },
+        ],
+        tulossaLeffat:[
+          {
+            id: 1,
+            title: "Mission: Impossible - The Final Reckoning",
+            poster:
+              "https://media.finnkino.fi/1012/Event_14435/portrait_medium/MissionImpossibleTFR_1080.jpg",
+          },
+          {
+            id: 2,
+            title: "Karate Kid: Legends",
+            poster:
+              "https://media.finnkino.fi/1012/Event_14477/portrait_medium/KarateKidLegends_1080.jpg",
+          },
+        ]
+      })
+    }, 1000)
+
+    return () => clearTimeout(timerId)
+  })
+
 
   return (
     <>
       <h3>Teatterissa</h3>
-      <MoviesList movies={inTheaters} />
+      <MoviesList movies={movies.ohjelmistossaNyt} />
 
       <h3>Tulossa teattereihin</h3>
-      <MoviesList movies={tulossaLeffat} />
+      <MoviesList movies={movies.tulossaLeffat} />
     </>
   );
 }
