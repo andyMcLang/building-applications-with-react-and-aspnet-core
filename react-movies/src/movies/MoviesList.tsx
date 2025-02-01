5ce3eaf520874810f296cf09d3224305d94d1f30
Andy Lang <djdreviz@gmail.com>
2025-02-01 03:46:13 +0200

    64. Generic List Component done!

--- react-movies/src/movies/MoviesList.tsx
+++ react-movies/src/movies/MoviesList.tsx
@@ -1,24 +1,20 @@
+import GenericList from "../utils/GenericList";
 import Loading from "../utils/Loading";
 import IndividualMovie from "./IndividualMovie";
 import { movieDTO } from "./movies.model";
 import css from "./MoviesList.module.css";
 
 export default function MoviesList(props: moviesListProps) {
-  if (!props.movies) {
-    return <Loading />;
-  } else if (props.movies.length === 0) {
-    return <>Ei ole leffoja näytettäväksi, sori!</>;
-  } else {
-    return (
-      <>
-        <div className={css.div}>
-          {props.movies.map((movie) => (
-            <IndividualMovie {...movie} key={movie.id} />
-          ))}
-        </div>
-      </>
-    );
-  }
+  return (
+    <GenericList 
+    list={props.movies}>
+      <div className={css.div}>
+        {props.movies?.map((movie) => (
+          <IndividualMovie {...movie} key={movie.id} />
+        ))}
+      </div>
+    </GenericList>
+  );
 }
 
 interface moviesListProps {
