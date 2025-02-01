c19c0a37713009bedbcb386da649f81f6952e880
Andy Lang <djdreviz@gmail.com>
2025-02-01 02:35:33 +0200

    61. Individual Movie Component done!

--- react-movies/src/App.tsx
+++ react-movies/src/App.tsx
@@ -1,9 +1,17 @@
 import "./App.css";
+import IndividualMovie from "./movies/IndividualMovie";
+import { movieDTO } from "./movies/movies.model";
 
 function App() {
+  const testMovie: movieDTO = {
+    id: 1,
+    title: "No Time To Die",
+    poster: "https://cdn.europosters.eu/image/750webp/114389.webp",
+  };
+
   return (
     <>
-      <h2>Hello App!</h2>
+      <IndividualMovie {...testMovie} />
     </>
   );
 }
