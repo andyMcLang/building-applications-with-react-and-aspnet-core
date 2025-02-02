45ecbc4af7c11f8ec5a870e276f7169a60b3dc84
Andy Lang <djdreviz@gmail.com>
2025-02-02 04:06:30 +0200

    72. Using JavaScript for Navigation Done!

--- react-movies/src/genres/CreateGenre.tsx
+++ react-movies/src/genres/CreateGenre.tsx
@@ -1,8 +1,20 @@
 
+import { useNavigate } from "react-router-dom";
+import Button from "../utils/Button";
+
 export default function CreateGenre() {
+  const navigate = useNavigate();
   return (
     <>
       <h3>Luodaan Genre</h3>
+      <Button
+        onClick={() => {
+          // ... tallennetaan tietokantaan
+          navigate("/genres");
+        }}
+      >
+        Tallenna tiedot
+      </Button>
     </>
   );
 }
