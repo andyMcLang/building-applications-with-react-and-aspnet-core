46e7871d9c1ee7fa7b394cff0b4e2d758897edd2
Andy Lang <djdreviz@gmail.com>
2025-02-03 00:28:00 +0200

    78. Building Our First Form done!

--- react-movies/src/genres/CreateGenre.tsx
+++ react-movies/src/genres/CreateGenre.tsx
@@ -1,20 +1,41 @@
-
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Button from "../utils/Button";
+import { Formik } from "formik";
 
 export default function CreateGenre() {
-  const navigate = useNavigate();
   return (
     <>
       <h3>Luodaan Genre</h3>
-      <Button
-        onClick={() => {
-          // ... tallennetaan tietokantaan
-          navigate("/genres");
+
+      <Formik
+        initialValues={{
+          name: "",
+        }}
+        onSubmit={(value) => {
+          // kun lomake on postattu
+          console.log(value);
         }}
       >
-        Tallenna tiedot
-      </Button>
+        {({ handleSubmit, handleChange, values }) => (
+          <form onSubmit={handleSubmit}>
+            <div className="mb-3">
+              <label htmlFor="name">Nimi</label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                className="form-control"
+                onChange={handleChange}
+                value={values.name} // Liitä input Formikin arvoon
+              />
+            </div>
+            <Button type="submit">Tallenna tiedot</Button>
+            <Link className="btn btn-secondary" to="/genres">
+              Peruuta
+            </Link>
+          </form>
+        )}
+      </Formik>
     </>
   );
 }
--- react-movies/src/utils/Button.tsx
+++ react-movies/src/utils/Button.tsx
@@ -1,13 +1,21 @@
-
 export default function Button(props: buttonProps) {
   return (
-    <button className="btn btn-primary" 
-    onClick={props.onClick}
-    >{props.children}</button>
+    <button
+      type={props.type}
+      className="btn btn-primary"
+      onClick={props.onClick}
+    >
+      {props.children}
+    </button>
   );
 }
 
 interface buttonProps {
   children: React.ReactNode;
-  onClick(): void;
+  onClick?(): void;
+  type: "button" | "submit";
 }
+
+Button.defaultProps = {
+  type: "button",
+};
