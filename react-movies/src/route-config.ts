196a88676a68c77a7326193bb5a6817faf4a341b
Andy Lang <djdreviz@gmail.com>
2025-02-02 03:01:11 +0200

    70. Route Configuration in an External File done!

---
+++ react-movies/src/route-config.ts
@@ -0,0 +1,16 @@
+import IndexGenres from "./genres/IndexGenres";
+import LandingPage from "./movies/LandingPage";
+
+const routes = [
+  {
+    path: "/genres",
+    component: IndexGenres,
+  },
+  {
+    path: "/",
+    component: LandingPage, 
+    exact: true
+  },
+];
+
+export default routes;
