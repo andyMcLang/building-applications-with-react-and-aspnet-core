8c0e22ca061c786a293e05fa34faaf94862629a1
Andy Lang <djdreviz@gmail.com>
2025-02-03 02:00:55 +0200

    81. Disabling Button When Submitting the Form done!

--- react-movies/src/forms/TextField.tsx
+++ react-movies/src/forms/TextField.tsx
@@ -1,20 +1,21 @@
 import { ErrorMessage, useField } from "formik";
 
 interface TextFieldProps {
-    field: string;
-    displayName: string;
-  }
+  field: string;
+  displayName?: string;
+}
 
 export default function TextField({ field, displayName }: TextFieldProps) {
-const [inputProps, meta] = useField(field);
+  const [inputProps, meta] = useField(field);
+  const labelText = displayName || field;
 
   return (
     <>
       <div className="mb-3">
-        <label htmlFor={field}>{displayName}</label>
+        <label htmlFor={field}>{labelText}</label>
         <input
           type="text"
-         {...inputProps} // Spread inputProps (sisältää onChange, onBlur, value jne.)
+          {...inputProps} // Sisältää onChange, onBlur, value jne.
           id={field}
           className="form-control"
         />
@@ -25,4 +26,3 @@ const [inputProps, meta] = useField(field);
     </>
   );
 }
-
