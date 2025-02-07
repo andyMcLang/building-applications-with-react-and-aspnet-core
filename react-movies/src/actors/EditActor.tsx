70e48abd6afa3527b08cda4792f540b7e5d83599
andy Lang <djdreviz@gmail.com>
2025-02-07 03:57:17 +0200

    84. Creating the Actors Form done!

--- react-movies/src/actors/EditActor.tsx
+++ react-movies/src/actors/EditActor.tsx
@@ -1,9 +1,15 @@
+import ActorForm from "./ActorForm";
 
 export default function EditActor() {
-    return (
-      <>
-        <h3>Muokataan Näyttelijätiedot</h3>
-      </>
-    );
-  }
-  
+  return (
+    <>
+      <h3>Muokataan Näyttelijätiedot</h3>
+      <ActorForm
+        model={{ name: "Tom Cruise", dateOfBirth: new Date("1962-07-03T00:00:00") }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      />
+    </>
+  );
+}
