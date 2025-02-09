ea345cef56541beff08fd9292d2a84a8cb2a6b12
Andy Lang <djdreviz@gmail.com>
2025-02-09 02:09:22 +0200

    87. Formatted Text with Markdown done!

--- react-movies/src/actors/EditActor.tsx
+++ react-movies/src/actors/EditActor.tsx
@@ -5,9 +5,14 @@ export default function EditActor() {
     <>
       <h3>Muokataan Näyttelijätiedot</h3>
       <ActorForm
-        model={{ name: "Tom Cruise", dateOfBirth: new Date("1962-07-03T00:00:00"),
-          pictureURL: 'https://upload.wikimedia.org/wikipedia/commons/9/98/Tom_Cruise_%2834450932580%29.jpg'
-         }}
+        model={{
+          name: "Tom Cruise",
+          dateOfBirth: new Date("1962-07-03T00:00:00"),
+          biography: `# jotain
+          Tämä henkilö on syntynyt **DR**`,
+          pictureURL:
+            "https://upload.wikimedia.org/wikipedia/commons/9/98/Tom_Cruise_%2834450932580%29.jpg",
+        }}
         onSubmit={(values) => {
           console.log(values);
         }}
