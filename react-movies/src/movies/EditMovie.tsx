d3a7134637ceecc6849a68d85a8b02cb89a91567
andy Lang <djdreviz@gmail.com>
2025-02-13 21:57:50 +0200

    93. Relating Movies with Genres and Movie Theaters done!

--- react-movies/src/movies/EditMovie.tsx
+++ react-movies/src/movies/EditMovie.tsx
@@ -1,6 +1,29 @@
+import { genreDTO } from "../genres/genres.model";
+import { movieTheaterDTO } from "../movietheaters/movieTheater.model";
 import MovieForm from "./MovieForm";
 
 export default function EditMovie() {
+
+  const nonSelectedGenres: genreDTO[] = [
+    { id: 2, name: "Komedia" },
+  ];
+
+  const selectedGenres: genreDTO[] = [
+    { id: 1, name: "Toiminta" }
+  ];
+
+  
+  const nonSelectedMovieTheaters: movieTheaterDTO[] = [
+    
+    { id: 2, name: "Kinopalatsi" },
+  ];
+
+  
+  const selectedMovieTheaters: movieTheaterDTO[] = [
+    { id: 1, name: "Tennispalatsi" }
+   
+  ];
+
   return (
     <>
       <h3>Muokataan Elokuvan Tiedot</h3>
@@ -9,6 +32,11 @@ export default function EditMovie() {
           releaseDate: new Date('2025-02-12T00:00:00')
          }}
         onSubmit={(values) => console.log(values)}
+        nonSelectedGenres={nonSelectedGenres}
+        selectedGenres={selectedGenres}
+
+        nonSelectedMovieTheaters={nonSelectedMovieTheaters}
+        selectedMovieTheaters={selectedMovieTheaters}
       />
     </>
   );
