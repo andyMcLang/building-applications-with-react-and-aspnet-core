bcdfeaac53d10d2db86f01fb917919e22efb6b09
Andy Lang <djdreviz@gmail.com>
2025-02-25 00:25:21 +0200

    101. Creating the Repository

---
+++ MoviesAPI/MoviesAPI/Controllers/GenresController.cs
@@ -0,0 +1,13 @@
+﻿using MoviesAPI.Services;
+
+namespace MoviesAPI.Controllers
+{
+    public class GenresController
+    {
+        private readonly IRepository repository;
+        public GenresController(IRepository repository)
+        {
+            this.repository = repository;
+        }
+    }
+}
