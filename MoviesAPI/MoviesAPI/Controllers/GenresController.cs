27f3b3aaf233b1a0b337829db963145ae23010d8
Andy Lang <djdreviz@gmail.com>
2025-03-21 00:21:46 +0200

    102. Controllers and Actions done!

--- MoviesAPI/MoviesAPI/Controllers/GenresController.cs
+++ MoviesAPI/MoviesAPI/Controllers/GenresController.cs
@@ -1,13 +1,52 @@
-﻿using MoviesAPI.Services;
+﻿// Test push repository from Visual Studio to GitHub
+using Microsoft.AspNetCore.Mvc;
+using MoviesAPI.Entities;
+using MoviesAPI.Services;
 
 namespace MoviesAPI.Controllers
 {
-    public class GenresController
+    [Route("api/genres")]
+    public class GenresController: ControllerBase
     {
         private readonly IRepository repository;
         public GenresController(IRepository repository)
         {
             this.repository = repository;
+        }
+
+        [HttpGet]
+        public List<Genre> Get()
+        {
+            return repository.GetAllGenres();
+        }
+
+        [HttpGet]
+        public Genre Get(int id)
+        {
+            var genre = repository.GetGenreById(id);
+            if (genre == null)
+            {
+                // return NotFound();
+            }
+            return genre;
+        }
+
+        [HttpPost]
+        public void Post()
+        {
+            // Add a new genre
+        }
+
+        [HttpPut]
+        public void Put()
+        {
+            // Update a genre
+        }
+
+        [HttpDelete]
+        public void Delete()
+        {
+            // Delete a genre
         }
     }
 }
