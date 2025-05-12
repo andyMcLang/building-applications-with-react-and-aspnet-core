d933590aad70d67770288060547a52a4e86679cb
Andy Lang <djdreviz@gmail.com>
2025-05-13 01:22:38 +0300

    108. Custom Validation done!

--- MoviesAPI/MoviesAPI/Entities/Genre.cs
+++ MoviesAPI/MoviesAPI/Entities/Genre.cs
@@ -1,12 +1,14 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using MoviesAPI.Validations;
+using System.ComponentModel.DataAnnotations;
 
 namespace MoviesAPI.Entities
 {
-    public class Genre
+    public class Genre : IValidatableObject
     {
         public int Id { get; set; }
-        [Required(ErrorMessage ="Alue {0} täytyy kirjoittaa")]
+        [Required(ErrorMessage = "Alue {0} täytyy kirjoittaa")]
         [StringLength(10)]
+        //[FirstLetterUppercase]
         public string Name { get; set; }
 
         [Range(18, 120)]
@@ -15,5 +17,18 @@ namespace MoviesAPI.Entities
         public string CreditCard { get; set; }
         [Url]
         public string Url { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!string.IsNullOrEmpty(Name))
+            {
+                var firstLetter = Name[0].ToString();
+
+                if (firstLetter != firstLetter.ToUpper())
+                {
+                    yield return new ValidationResult("Eka kirjain täytyy olla isolla!", new string[] { nameof(Name) });
+                }
+            }
+        }
     }
 }
