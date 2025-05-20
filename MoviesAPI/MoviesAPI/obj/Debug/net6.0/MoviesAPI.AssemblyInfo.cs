90f1ebbb1a920275437999b40c3301191e667fe0
Andy Lang <djdreviz@gmail.com>
2025-05-21 00:22:54 +0300

    118. Axios, Environment Variables and CORS port fixed!

--- MoviesAPI/MoviesAPI/obj/Debug/net6.0/MoviesAPI.AssemblyInfo.cs
+++ MoviesAPI/MoviesAPI/obj/Debug/net6.0/MoviesAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MoviesAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6d2bda671190516bd26a6ded4e8fb5da5c95bb9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59f2a11926fdd1368adfeff39ca54a8bd80805b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MoviesAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MoviesAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
