e46e4b6413b1008509b50ae201738a0b36b4b0b4
Andy Lang <djdreviz@gmail.com>
2025-05-21 12:11:15 +0300

    122. Genrejen luominen - React done

--- MoviesAPI/MoviesAPI/obj/Debug/net6.0/MoviesAPI.AssemblyInfo.cs
+++ MoviesAPI/MoviesAPI/obj/Debug/net6.0/MoviesAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MoviesAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31f5ed096d5873872685edbde472b5fd6fadcb63")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ebd5093f2c307c885cc415abac0bd5ab45a9081")]
 [assembly: System.Reflection.AssemblyProductAttribute("MoviesAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MoviesAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- react-movies/src/genres/CreateGenre.tsx
+++ react-movies/src/genres/CreateGenre.tsx
@@ -1,14 +1,28 @@
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 import GenreForm from "./GenreForm";
+import { genreCreationDTO } from "./genres.model";
+import { urlGenres } from "../endpoints";
 
 export default function CreateGenre() {
+  const navigate = useNavigate();
+
+  async function create(genre: genreCreationDTO) {
+    try {
+      await axios.post(urlGenres, genre);
+      navigate("/genres"); // vie takaisin genrelistaan onnistuneen lisäyksen jälkeen
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <>
       <h3>Luodaan uusi genre</h3>
       <GenreForm
         model={{ name: "" }}
         onSubmit={async (value) => {
-          await new Promise((r) => setTimeout(r, 400));
-          console.log(value);
+          await create(value);
         }}
       />
     </>
