11e2a90a535e58910982b5468f6369cbd147a30a
Andy Lang <djdreviz@gmail.com>
2025-05-28 18:30:01 +0300

    128. Paginating Genres done!

--- MoviesAPI/MoviesAPI/obj/Debug/net6.0/MoviesAPI.AssemblyInfo.cs
+++ MoviesAPI/MoviesAPI/obj/Debug/net6.0/MoviesAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MoviesAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+275b1dbbba616277a520cfd5103b802e6135496f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d354bbe89a04cae16d0d3556de62fc5567b5f7b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MoviesAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MoviesAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
