af3b5331ac17b8da8622d7002c782d0c237a1e39
Andy Lang <djdreviz@gmail.com>
2025-05-30 18:21:15 +0300

    131. Edit Entity done!

--- react-movies/src/genres/EditGenre.tsx
+++ react-movies/src/genres/EditGenre.tsx
@@ -1,51 +1,25 @@
-import { useParams, useNavigate } from "react-router-dom";
-import GenreForm from "./GenreForm";
-import { useEffect, useState } from "react";
 import { urlGenres } from "../endpoints";
-import axios, { AxiosResponse } from "axios";
-import { genreCreationDTO } from "./genres.model";
-import Loading from "../utils/Loading";
-import DisplayErrors from "../utils/DisplayErrors";
+import EditEntity from "../utils/EditEntity";
+import GenreForm from "./GenreForm";
+import { genreCreationDTO, genreDTO } from "./genres.model";
 
 export default function EditGenre() {
-  const { id }: any = useParams();
-  const [genre, setGenre] = useState<genreCreationDTO>();
-  const [errors, setErrors] = useState<string[]>([]);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    axios
-      .get(`${urlGenres}/${id}`)
-      .then((response: AxiosResponse<genreCreationDTO>) => {
-        setGenre(response.data);
-      });
-  }, [id]);
-
-  async function edit(genreToEdit: genreCreationDTO) {
-    try {
-      await axios.put(`${urlGenres}/${id}`, genreToEdit);
-      navigate("/genres");
-    } catch (error) {
-      if (error && error.response) {
-        setErrors(error.response.data);
-      }
-    }
-  }
-
   return (
     <>
-      <h3>Muokataan Genreä</h3>
-      <DisplayErrors errors={errors} />
-      {genre ? (
-        <GenreForm
-          model={{ name: "Action" }}
-          onSubmit={async (value) => {
-            await edit(value);
-          }}
-        />
-      ) : (
-        <Loading />
-      )}
+      <EditEntity<genreCreationDTO, genreDTO>
+        url={urlGenres}
+        entityName="Genres"
+        indexURL="/genres"
+      >
+        {(entity, edit) => (
+          <GenreForm
+            model={entity}
+            onSubmit={async (value) => {
+              await edit(value);
+            }}
+          />
+        )}
+      </EditEntity>
     </>
   );
 }
