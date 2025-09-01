ea92d0b42491b959d74dc6fa1a757b11dd70130f
Andy Lang <djdreviz@gmail.com>
2025-09-02 00:05:43 +0300

    154. Deleting a Movie done!

--- react-movies/src/movies/IndividualMovie.tsx
+++ react-movies/src/movies/IndividualMovie.tsx
@@ -1,9 +1,22 @@
 import { movieDTO } from "./movies.model";
 import css from "./IndividualMovie.module.css";
 import { Link } from "react-router-dom";
+import custonConfirm from "../utils/customConfirm";
+import Button from "../utils/Button";
+import { urlMovies } from "../endpoints";
+import axios from "axios";
+import { useContext } from "react";
+import AlertContext from "../utils/AlertContext";
 
 export default function IndividualMovie(props: movieDTO) {
   const buildLink = () => `/movie/${props.id}`;
+  const customAlert = useContext(AlertContext);
+
+  function deleteMovie() {
+    axios.delete(`${urlMovies}/${props.id}`).then(() => {
+      customAlert();
+    });
+  }
 
   return (
     <div className={css.div}>
@@ -13,6 +26,21 @@ export default function IndividualMovie(props: movieDTO) {
       <p>
         <Link to={buildLink()}>{props.title}</Link>
       </p>
+      <div>
+        <Link
+          style={{ marginRight: "1rem" }}
+          className="btn btn-info"
+          to={`/movie/edit/${props.id}`}
+        >
+          Muokkaa
+        </Link>
+        <Button
+          onClick={() => custonConfirm(() => deleteMovie())}
+          className="btn btn-danger"
+        >
+          Poista
+        </Button>
+      </div>
     </div>
   );
 }
--- react-movies/src/movies/LandingPage.tsx
+++ react-movies/src/movies/LandingPage.tsx
@@ -3,6 +3,7 @@ import MoviesList from "./MoviesList";
 import { landingPageDTO } from "./movies.model";
 import { urlMovies } from "../endpoints";
 import axios, { AxiosResponse } from "axios";
+import AlertContext from "../utils/AlertContext";
 
 export default function LandingPage() {
   const [movies, setMovies] = useState<landingPageDTO>({
@@ -11,20 +12,28 @@ export default function LandingPage() {
   });
 
   useEffect(() => {
+    loadData();
+  }, []);
+
+  function loadData() {
     axios.get(urlMovies).then((response: AxiosResponse<any>) => {
       setMovies({
         ohjelmistossaNyt: response.data.inTheaters ?? [],
         tulossaLeffat: response.data.upcomingReleases ?? [],
       });
     });
-  }, []);
+  }
 
   return (
-    <>
+    <AlertContext.Provider
+      value={() => {
+        loadData();
+      }}
+    >
       <h3>Teatterissa</h3>
       <MoviesList movies={movies.ohjelmistossaNyt || []} /> {}
       <h3>Tulossa teattereihin</h3>
       <MoviesList movies={movies.tulossaLeffat || []} /> {}
-    </>
+    </AlertContext.Provider>
   );
 }
